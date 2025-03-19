4d5bfdf84d298551e5a557f688831074e3333b24
Travis m <murdocktravis03@gmail.com>
2025-03-19 09:22:21 -0700

    teliport

--- Assets/Josh/Scripts/LevelTeleport.cs
+++ Assets/Josh/Scripts/LevelTeleport.cs
@@ -5,22 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelTeleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string levelName;
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(levelName);
         }
     }
+    
 }
