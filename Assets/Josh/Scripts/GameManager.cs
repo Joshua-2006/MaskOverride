c2355dff8276b1b13f6e53f6fb15b660ae4a66ed
Joshua Jopling <149619818+Joshua-2006@users.noreply.github.com>
2025-04-17 10:19:58 -0700

    fff

--- Assets/Josh/Scripts/GameManager.cs
+++ Assets/Josh/Scripts/GameManager.cs
@@ -19,7 +19,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ammo = 10;
     }
 
     // Update is called once per frame
